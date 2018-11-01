749d9c36ae1a14609d67ce4300d47d2bcd40d3bc
Michael Ringenberg <ringenberg@cmu.edu>
2018-11-01 14:30:17 -0400

    Uses easyui for cleaner interface.

--- classroom/src/app/app.component.ts
+++ classroom/src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { AppSettingsService } from './app-settings.service';
-import { ModalService } from './modal.service';
 
 @Component({
   selector: 'app-root',
@@ -11,7 +10,6 @@ import { ModalService } from './modal.service';
 export class AppComponent {
   title = 'DocuScope Classroom @ CMU';
 
-  constructor(private modalService: ModalService) { }
-  openModal(id: string) { this.modalService.open(id); }
-  closeModal(id: string) { this.modalService.close(id); }
+  constructor() { }
+
 }
