a43a110719c92d1dcb2ab0ba7a1fce999c24bd3c
Michael Ringenberg <ringenberg@cmu.edu>
2019-01-28 13:45:44 -0500

    Updates to new base image to try to fix #12. Cleans up default_settings and its usage.

--- app/create_app.py
+++ app/create_app.py
@@ -10,7 +10,7 @@ def create_flask_app():
     """Create and initialize the Flask application."""
     app = Flask(__name__)
     app.config.from_object(Config)
-    app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+mysqldb://{0.dbUsername}:{0.dbPassword}@{0.dbHost}:{0.dbPort}/{0.dbName}".format(Config)
+    #app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+mysqldb://{0.dbUsername}:{0.dbPassword}@{0.dbHost}:{0.dbPort}/{0.dbName}".format(Config)
     db.init_app(app)
     #db.Model.metadata.reflect(db.get_engine(app=app))
     return app
--- app/default_settings.py
+++ app/default_settings.py
@@ -1,17 +1,23 @@
 """Configuration properties that are retrieved from the environment
  and their default settings."""
 import os
+from pathlib import Path
+
+def get_secret(env_var, default=None):
+    """Retrieves the value of the given environment variable prefering any
+    {env_var}_FILE variation to work with docker secrets."""
+    efile = os.getenv("{}_FILE".format(env_var))
+    return Path(efile).read_text().strip() if efile else os.getenv(env_var, default)
+
 
 class Config():
     """Configuration object used as part of initializing this Flask app."""
     DICTIONARY_SERVER = os.getenv('DICTIONARY_SERVER', 'http://dictionary')
-    dbHost = os.getenv('dbHost', '192.168.37.135')
-    dbPort = os.getenv('dbPort', '13306')
-    dbName = os.getenv('dbName', 'docuscope')
-    dbTable = os.getenv('dbTable', 'ds_documents')
-    dbUsername = os.getenv('dbUsername', 'unity')
-    dbPassword = os.getenv('dbPassword', '4570WK821X6OdfyT508srN09wV')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    COUCHDB_USER = os.getenv('COUCHDB_USER', 'guest')
-    COUCHDB_PASSWORD = os.getenv('COUCHDB_PASSWORD', 'guest')
-    COUCHDB_URL = os.getenv('COUCHDB_URL', 'http://couchdb:5984')
+    SQLALCHEMY_DATABASE_URI = "{prot}://{user}:{passwd}@{host}:{port}/{database}".format(
+        prot='mysql+mysqldb',
+        user=get_secret('MYSQL_USER', 'root'),
+        passwd=get_secret('MYSQL_PASSWORD', 'rootpw'),
+        host=os.getenv('DB_HOST', 'mysql'),
+        port=os.getenv('DB_PORT', '3306'),
+        database=os.getenv('MYSQL_DATABASE', 'docuscope'))
