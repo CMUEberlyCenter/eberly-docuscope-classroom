675fddda3da0a5498a34d1dbb6faf35e0f07b485
Michael Ringenberg <ringenberg@cmu.edu>
2019-03-20 12:14:57 -0400

    Experiments with WhiteNoise

--- app/create_app.py
+++ app/create_app.py
@@ -2,18 +2,15 @@
 from flask import Flask
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-#from flask_sqlalchemy import SQLAlchemy
+#from whitenoise import WhiteNoise
 
 from default_settings import Config
-
-#db = SQLAlchemy()
 
 def create_flask_app():
     """Create and initialize the Flask application."""
     app = Flask(__name__)
     app.config.from_object(Config)
-    engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
-    app.Session = sessionmaker(bind=engine)
-    #db.init_app(app)
-    #db.Model.metadata.reflect(db.get_engine(app=app))
+    app.engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+    app.Session = sessionmaker(bind=app.engine)
+    #app.wsgi_app = WhiteNoise(app.wsgi_app, root='static/', index_file=True)
     return app
