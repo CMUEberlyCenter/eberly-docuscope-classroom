253503a13b48f783c7dcdd8f31be637638f220e9
Michael Ringenberg <ringenberg@cmu.edu>
2019-03-21 13:47:33 -0400

    Improves instructor check for #14

--- classroom/src/app/nav/nav.component.ts
+++ classroom/src/app/nav/nav.component.ts
@@ -15,7 +15,7 @@ export class NavComponent implements OnInit {
   }
   is_instructor() {
     const qmap = this._route.snapshot.queryParamMap;
-    return qmap.has('rolls') && qmap.get('rolls').search(/Instructor/i) >= 0;
+    return qmap && qmap.has('rolls') && qmap.get('rolls').search(/Instructor/i) >= 0;
   }
   ngOnInit() { }
 
