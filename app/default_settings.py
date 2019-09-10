5b77bc8ce6b707d6f6582c4d590cfa7af2c1675e
Michael Ringenberg <ringenberg@cmu.edu>
2019-09-10 14:09:26 -0400

    Adds charset option to mysql URI to force utf8 encoding.

--- app/default_settings.py
+++ app/default_settings.py
@@ -14,7 +14,7 @@ class Config(): #pylint: disable=R0903
     """Configuration object used as part of initializing this Flask app."""
     DICTIONARY_HOME = os.getenv('DICTIONARY_HOME', os.path.join('/app', 'dictionaries'))
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = "{prot}://{user}:{passwd}@{host}:{port}/{database}".format(
+    SQLALCHEMY_DATABASE_URI = "{prot}://{user}:{passwd}@{host}:{port}/{database}?charset=utf8mb4".format(
         prot='mysql+mysqldb',
         user=get_secret('MYSQL_USER', 'root'),
         passwd=get_secret('MYSQL_PASSWORD', 'rootpw'),
