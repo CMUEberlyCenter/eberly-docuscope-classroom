8aad61c55ba7897a8da1baaba22ab247be0dc305
Michael Ringenberg <ringenberg@cmu.edu>
2019-09-25 13:51:02 -0400

    Reverting changes, it looks like the problem is with the docker image used to run chrome headless.  I suspect there is a missing library.

--- classroom/src/karma.conf.js
+++ classroom/src/karma.conf.js
@@ -13,8 +13,7 @@ module.exports = function (config) {
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
-      clearContext: false, // leave Jasmine Spec Runner output visible in browser
-      args: ['--no-sandbox']
+      clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, '../coverage'),
