3be30666c14a348036bb8a252521fe7349a1fa45
Michael Ringenberg <mringenb@andrew.cmu.edu>
2020-11-13 12:26:08 -0500

    Fixes errors in testing code.

--- classroom/src/app/category-select/category-select.component.spec.ts
+++ classroom/src/app/category-select/category-select.component.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CategorySelectComponent } from './category-select.component';
@@ -8,7 +9,8 @@ describe('CategorySelectComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CategorySelectComponent ]
+      declarations: [ CategorySelectComponent ],
+      imports: [ HttpClientTestingModule ]
     })
     .compileComponents();
   });
--- classroom/src/app/dictionary-tree.service.spec.ts
+++ classroom/src/app/dictionary-tree.service.spec.ts
@@ -1,4 +1,6 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { MatMenuModule } from '@angular/material/menu';
 
 import { DictionaryTreeService } from './dictionary-tree.service';
 
@@ -6,7 +8,9 @@ describe('DictionaryTreeService', () => {
   let service: DictionaryTreeService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [ HttpClientTestingModule, MatMenuModule ]
+    });
     service = TestBed.inject(DictionaryTreeService);
   });
 
