d00d6f3ad466ae3c4fcb053e0c0eb9ca61288e31
Michael Ringenberg <mringenb@andrew.cmu.edu>
2021-07-13 14:48:53 -0400

    Adds scale legend. Uses sqrt scale.

--- classroom/src/app/bubble-chart/bubble-chart.component.ts
+++ classroom/src/app/bubble-chart/bubble-chart.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import * as d3 from 'd3';
 import { NgxUiLoaderService } from 'ngx-ui-loader';
 import { forkJoin } from 'rxjs';
 import { AssignmentService } from '../assignment.service';
@@ -10,8 +11,10 @@ import {
   DocumentData,
   DsDataService,
 } from '../ds-data.service';
+import { SettingsService } from '../settings.service';
 
 interface ICell {
+  proportion: number;
   title: string;
   value: number;
   category: string;
@@ -28,7 +31,20 @@ export class BubbleChartComponent implements OnInit {
   dictionary: CommonDictionary | undefined;
   data: DocumentData[] = [];
   maxRadius = 20;
-  get maxValue(): number {
+  maxValue = 0;
+  scale!: d3.ScaleLinear<number, number, never>;
+  unit = 100;
+
+  constructor(
+    private assignmentService: AssignmentService,
+    private commonDictionaryService: CommonDictionaryService,
+    private corpusService: CorpusService,
+    private dataService: DsDataService,
+    private settingsService: SettingsService,
+    private spinner: NgxUiLoaderService
+  ) {}
+
+  calcMaxValue(): number {
     const maxValue = Math.max(
       ...this.data.map((doc) =>
         Math.max(
@@ -41,25 +57,26 @@ export class BubbleChartComponent implements OnInit {
     return maxValue > 0 ? maxValue : 1;
   }
 
-  constructor(
-    private assignmentService: AssignmentService,
-    private commonDictionaryService: CommonDictionaryService,
-    private corpusService: CorpusService,
-    private dataService: DsDataService,
-    private spinner: NgxUiLoaderService
-  ) {}
-
   // eslint-disable-next-line @angular-eslint/no-empty-lifecycle-method
   ngOnInit(): void {
     this.spinner.start();
     this.corpusService.getCorpus().subscribe((corpus) => {
       this.corpus = corpus;
       return forkJoin([
+        this.settingsService.getSettings(),
         this.commonDictionaryService.getJSON(),
         this.dataService.getData(corpus),
-      ]).subscribe(([common, data]) => {
+      ]).subscribe(([settings, common, data]) => {
+        this.unit = settings.unit;
         this.dictionary = common;
         this.data = data.data;
+        this.maxValue = this.calcMaxValue();
+        this.scale = d3
+          .scaleSqrt()
+          //.scaleLinear()
+          .domain([0, this.maxValue])
+          .range([0, this.maxRadius])
+          .nice();
         this.assignmentService.setAssignmentData(data);
         this.spinner.stop();
       });
@@ -76,11 +93,21 @@ export class BubbleChartComponent implements OnInit {
     return category_value(category.name ?? category.label, doc) / this.maxValue;
   }
   getCell(doc: DocumentData, category: Entry): ICell {
+    const value = category_value(category.name ?? category.label, doc);
     return {
       title: doc.title,
-      value: this.getValue(doc, category),
+      value: value * this.unit,
+      proportion: this.scale(value),
       category: category.name ?? category.label,
     };
+  }
+  lscale(i: number): number {
+    return (
+      this.scale
+        .ticks(4)
+        .slice(1, 1 + i)
+        .reduce((p, t) => p + this.scale(t), 0) * 2
+    );
   }
   open(doc_id: string): void {
     if (doc_id) {
