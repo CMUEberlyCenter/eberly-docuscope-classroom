ccfca19a3f25b18dd3bd7f250676e9657e12934a
Michael Ringenberg <mringenb@andrew.cmu.edu>
2021-07-15 12:48:34 -0400

    Adds level selection.

--- classroom/src/app/bubble-chart/bubble-chart.component.ts
+++ classroom/src/app/bubble-chart/bubble-chart.component.ts
@@ -1,4 +1,6 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 import * as d3 from 'd3';
 import { NgxUiLoaderService } from 'ngx-ui-loader';
 import { forkJoin } from 'rxjs';
@@ -27,15 +29,26 @@ interface ICell {
 })
 export class BubbleChartComponent implements OnInit {
   @ViewChild('bubble') bubble!: ElementRef;
+  @ViewChild('documentSort') sort: MatSort;
+  columns: string[] = [];
   corpus: string[] = [];
+  #depth = 'Category';
   dictionary: CommonDictionary | undefined;
   data: DocumentData[] = [];
   maxRadius = 20;
   maxValue = 0;
   scale!: d3.ScaleLinear<number, number, never>;
   stickyHeader = true;
+  tableData: MatTableDataSource<DocumentData> | undefined;
   unit = 100;
 
+  get depth(): string {
+    return this.#depth;
+  }
+  set depth(d: string) {
+    this.#depth = d;
+    this.columns = this.genColumns();
+  }
   constructor(
     private assignmentService: AssignmentService,
     private commonDictionaryService: CommonDictionaryService,
@@ -71,7 +84,10 @@ export class BubbleChartComponent implements OnInit {
         this.unit = settings.unit;
         this.stickyHeader = settings.sticky_headers;
         this.dictionary = common;
+        this.columns = this.genColumns();
         this.data = data.data;
+        this.tableData = new MatTableDataSource(this.data);
+        this.tableData.sort = this.sort;
         this.maxValue = this.calcMaxValue();
         this.scale = d3
           .scaleSqrt()
@@ -84,15 +100,48 @@ export class BubbleChartComponent implements OnInit {
       });
     });
   }
+  /*ngAfterViewChecked(): void {
+    if (this.dictionary && this.data) {
+      this.tableSort.sort = this.sort;
+    }
+  }*/
 
-  get columns(): string[] {
+  genColumns(): string[] {
+    let cols: string[] = [];
+    if (this.dictionary) {
+      if (this.depth == 'Category') {
+        cols = this.dictionary.categories.map((c) => c.name ?? c.label);
+      } else if (this.depth == 'Subcategory') {
+        cols = this.dictionary.categories.reduce(
+          (a, c) => [...a, ...c.subcategories.map((s) => s.name ?? s.label)],
+          [] as string[]
+        );
+      } else if (this.depth == 'Cluster') {
+        cols = this.dictionary.categories.reduce(
+          (a, c) => [
+            ...a,
+            ...c.subcategories.reduce(
+              (sa, sub) => [
+                ...sa,
+                ...sub.clusters.map((cl) => cl.name ?? cl.label),
+              ],
+              [] as string[]
+            ),
+          ],
+          [] as string[]
+        );
+      }
+    }
     return [
       'name',
-      ...this.dictionary.categories.map((c) => c.name ?? c.label),
+      ...cols,
+      //...this.dictionary.categories.map((c) => c.name ?? c.label),
+      //...this.dictionary.nodes.map((c) => c.name ?? c.label),
     ];
   }
   getCell(doc: DocumentData, category: Entry): ICell {
     const value = category_value(category.name ?? category.label, doc);
+    //const value = category_value(category.id, doc);
     return {
       title: doc.title,
       value: value * this.unit,
